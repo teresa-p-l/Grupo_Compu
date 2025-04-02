fcae2afbac41d56cde381f2a17cc483ae8f8a7ed
teresa-p-l <teresapulop@gmail.com>
2025-04-02 12:54:05 +0200

    le he quitado morralla

--- hola.cpp
+++ hola.cpp
@@ -1,25 +1,11 @@
-/*Hola*/
-
-//pa ti mi cola
-
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <cmath>
-using namespace std;
-
-<<<<<<< HEAD
-=======
-//pa ti mi cola
-
 #include <stdio.h>
-
+using namespace std;
 int main() {
     cout << ("Hola mundo");
     return 0;
 }
-
-/*Odio a gente que no se llama Diego*/
-
 
