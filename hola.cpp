10542103d646e3a177dd30ad5be097ad845ffd79
DiegoConol <diegoconolinsti@gmail.com>
2025-04-03 12:15:50 +0200

    arreglao

--- hola.cpp
+++ hola.cpp
@@ -2,15 +2,14 @@
 #include <string>
 #include <vector>
 #include <cmath>
-<<<<<<< HEAD
+
 using namespace std;
 
 //<<<<<<< HEAD
 //=======
 //pa ti mi cola
 
-=======
->>>>>>> cc3f67534beeef32c8c5f8fcd67b67b4cb8b37d2
+
 #include <stdio.h>
 using namespace std;
 int main() {
